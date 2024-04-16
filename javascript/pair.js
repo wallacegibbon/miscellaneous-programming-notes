292653ea3b92e56af152239d46d5bc2aeda2df63
wallacegibbon <wallacegibbon@aliyun.com>
2024-04-16 16:32:27 +0800

    Trivial improvement: Some usage examples are added to the closure-based pair/cons implementation in Javascript.

--- javascript/pair.js
+++ javascript/pair.js
@@ -10,3 +10,17 @@ export let length = ls => ls === null ? 0 : 1 + length(tail(ls));
 
 export let to_string = x => is_pair(x) ? `(${to_string(head(x))}, ${to_string(tail(x))})` : String(x);
 
+/// Usage examples:
+
+to_string(pair(1, pair(2, pair(3, null))));
+//> "(1, (2, (3, null)))"
+
+to_string(pair(1, pair(pair(pair(1, 5), 3), null)));
+//> "(1, (((1, 5), 3), null))"
+
+to_string(tail(pair(1, pair(pair(pair(1, 5), 3), null))));
+//> "(((1, 5), 3), null)"
+
+length(pair(1, pair(pair(pair(1, 5), 3), null)));
+//> 2
+
